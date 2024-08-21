2cd6f8481e4b24f753b984d988ad5e394e348e79
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-21 16:28:34 +0200

    Creata section reviews da fixare

--- main.js
+++ main.js
@@ -2,6 +2,11 @@
 let navbar = document.querySelector('#navbar');
 let btncollapse = document.querySelector('#btncollapse');
 let check = false; //variabile di appoggio per far ruotare il button della collapse personalizzato
+let reviews = [
+    { name: 'Riccardo', body: 'Adoro gli anime e questo sito mi piace molto', rank: 5},
+    { name: 'Roberto', body: 'Non mi piace per niente', rank: 1},
+    { name: 'Francesco', body: 'Così Così', rank: 3},
+]
 
 // Navbar settings
 window.addEventListener('scroll', ()=>{
@@ -44,7 +49,7 @@ function counters(n, element, time){
         }
 }, time);
 
-    setTimeout(() => {
+    setTimeout(()=> {
         timeoutcheck = true;
     }, 3000);
 };
@@ -64,3 +69,47 @@ let watcher = new IntersectionObserver( (entries)=>{
 })
 
 watcher.observe(firstNumber);
+
+
+// Reviews
+
+let swiperWrapper = document.querySelector('.swiper-wrapper');
+
+reviews.forEach((review)=>{
+    let div = document.createElement('div');
+    div.classList.add('swiper-slide');
+    div.innerHTML = `
+        <div class="cardreview d-flex flex-column justify-content-center align-items-center">
+            <p class="h4 text-center">${review.name}</p>
+            <p class="lead text-center"><strong>${review.body}</strong></p>
+            <div class="d-flex justify-content-center starwrapper">
+            <i class="fa-solid fa-star text-warning"></i>
+            <i class="fa-solid fa-star text-warning"></i>
+            <i class="fa-solid fa-star text-warning"></i>
+            <i class="fa-solid fa-star text-warning"></i>
+            <i class="fa-solid fa-star text-warning"></i>
+            </div>
+        </div>
+    `
+    swiperWrapper.appendChild(div);
+});
+
+
+
+
+
+
+
+
+// Swiper settings
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
