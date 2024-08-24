a45f4ea049169f6cc026b9942f5da9acbffb192f
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-24 15:52:55 +0200

    Create tutte le card e personalizzate

--- annunci.js
+++ annunci.js
@@ -1,5 +1,6 @@
 fetch('./annunci.json').then((response)=> response.json()).then((data)=>{
     let radioWrapper = document.querySelector('#radioWrapper');
+    let cardWrapper = document.querySelector('#cardWrapper');
 
     function radioCreate(){
         let categories = data.map( (annuncio)=> annuncio.category);
@@ -19,6 +20,26 @@ fetch('./annunci.json').then((response)=> response.json()).then((data)=>{
 
     }
 
-    radioCreate()
+    radioCreate();
+
+    function showCards(){
+        data.forEach((annuncio, i)=>{
+            let div = document.createElement('div');
+            div.classList.add('card', 'mx-1', 'mb-2');
+            div.innerHTML = `
+                <img src="https://picsum.photos/${200 + i}" class="card-img-top img-fluid" alt="Immagine rappresentativa del prodotto">
+                <div class="card-body">
+                    <h5 class="card-title">${annuncio.name}</h5>
+                    <p class="card-text">${annuncio.category}</p>
+                    <p class="lead">${annuncio.price}€</p>
+                    <a href="#" class="btn btn-custom2">Acquista</a>
+                </div>
+            `;
+            cardWrapper.appendChild(div);
+
+        })
+    }
+
+    showCards();
 
 })
