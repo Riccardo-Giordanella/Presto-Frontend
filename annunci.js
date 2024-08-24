7930ed85199a22db221d1f094f86ee55b877b56a
Riccardo Giordanella <it.gg@hotmail.it>
2024-08-24 11:39:06 +0200

    Creati i radio per le categories e la card annunci

--- annunci.js
+++ annunci.js
@@ -1,3 +1,24 @@
 fetch('./annunci.json').then((response)=> response.json()).then((data)=>{
+    let radioWrapper = document.querySelector('#radioWrapper');
+
+    function radioCreate(){
+        let categories = data.map( (annuncio)=> annuncio.category);
+        let uniqueCategories = Array.from(new Set(categories));
+        uniqueCategories.forEach((category)=>{
+            let div = document.createElement('div');
+            div.classList.add('form-check');
+            div.innerHTML = `
+                <input class="form-check-input" type="radio" name="flexRadioDefault" id="${category}">
+                <label class="form-check-label" for="${category}">
+                    ${category}
+                </label>
+            `;
+            radioWrapper.appendChild(div);
+        });
+
+
+    }
+
+    radioCreate()
 
 })
